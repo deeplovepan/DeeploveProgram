6f2c47052672b25e26696a37595aec79d91f874f
deeplove <deeplove@deeploves-Mac-mini.local>
2011-04-29 18:26:49 +0800

    commit

---
+++ JavaChangeType.java
@@ -0,0 +1,3 @@
+// int to String 
+ex:
+String stringValue = Integer.toString(12345);
---
+++ JavaFileAndIoEx.java
@@ -0,0 +1,7 @@
+// read every line from buffer reader
+BufferedReader buffer;
+String line;
+while( (line= buffer.readLine()) != null)
+{
+    
+}
