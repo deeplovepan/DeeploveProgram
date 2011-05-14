320627667300ac2650c8c1e42c9b38cb3d2100fb
deeplovepan <deeplovepan@gmail.com>
2011-05-14 19:28:51 +0800

    commit

---
+++ MyFacebook.h
@@ -0,0 +1,25 @@
+//
+//  MyFacebook.h
+//  FacebookTest
+//
+//  Created by deeplove on 2011/5/14.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FBConnect.h"
+
+
+@interface MyFacebook : NSObject <FBSessionDelegate, FBRequestDelegate>{
+    Facebook *facebook;
+
+}
+@property (nonatomic, retain) Facebook *facebook;
+
+-(void)login;
+-(void)publish:(NSString*)msg picLink:(NSString*)picLink;
+-(void)logout;
+-(BOOL)isUserLogin;
+-(void)like:(NSString*)objId;
+
+@end
---
+++ iosFacebookEx.h
@@ -0,0 +1,16 @@
+//
+//  iosFacebookEx.h
+//  DeeploveProgram
+//
+//  Created by deeplove on 2011/5/14.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface iosFacebookEx : NSObject {
+    
+}
+
+@end
