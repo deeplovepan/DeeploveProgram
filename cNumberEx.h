b066157eaca66df793e4cd889aee3c0e288c5e68
deeplove <deeplovepan@gmail.com>
2011-07-13 13:49:25 +0800

    commit

---
+++ cNumberEx.h
@@ -0,0 +1,7 @@
+//
+//  cNumberEx.h
+//  DeeploveProgram
+//
+//  Created by deeplove on 7/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
---
+++ cNumberLib.h
@@ -0,0 +1,9 @@
+//
+//  cNumberLib.h
+//  DeeploveProgram
+//
+//  Created by deeplove on 7/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+char isNumberInt(float num);
