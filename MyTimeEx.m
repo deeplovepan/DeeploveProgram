392843c7533d416c5b881fc37f684208b6657422
Peter Pan <deeplovepan@gmail.com>
2012-02-21 11:19:05 +0800

    add delete my attend event in my event page

---
+++ MyTimeEx.m
@@ -0,0 +1,24 @@
+//
+//  MyTimeEx.m
+//  DeeploveProgram
+//
+//  Created by Peter Pan on 2/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MyTimeEx.h"
+
+@implementation MyTimeEx
+
+/*
+     NSTimeZone* currentTimeZone = [NSTimeZone systemTimeZone]; 
+     NSLog(@"%@", [currentTimeZone name]);
+ 
+     ->
+ 
+     ex: Asia/Taipei
+*/
+
+
+
+@end
